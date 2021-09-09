454a54120ae01d2c57cbbacf47ca2be6a6d67f28
cd <coolezprogramming@gmail.com>
2021-09-09 19:15:21 -0400

    Basic functionalities added

--- script.js
+++ script.js
@@ -4,50 +4,68 @@ function computerPlay() {
     if (computerNum == 1) {
         return "rock";
     } else if (computerNum == 2) {
-        return "paper"
+        return "paper";
     } else {
         return "scissors";
     }
 }
 
-let playerSelection = " ";
-let computerSelection = " ";
 let computerScore = 0;
 let playerScore = 0;
 let plays = 0;
+const winnerAlert = document.getElementById("winnerAlert");
+document.getElementById("playerScore").innerHTML =
+    "Player Score: " + playerScore;
+
+document.getElementById("computerScore").innerHTML =
+    "Computer Score: " + computerScore;
+
+function updateScore() {
+    document.getElementById("playerScore").innerHTML =
+        "Player Score: " + playerScore;
+
+    document.getElementById("computerScore").innerHTML =
+        "Computer Score: " + computerScore;
+}
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection){
-        return "draw, try again";
-    } 
+    computerSelection = computerPlay();
+    console.log(playerSelection, computerSelection);
 
-    if (computerSelection == "rock" && playerSelection == "scissors" || computerSelection == "scissors" && playerSelection == "paper" || computerSelection == "paper" && playerSelection == "rock") {
-        computerScore++
-        plays++
+    if (playerSelection == computerSelection) {
+        winnerAlert.textContent = "draw, try again";
+        return 0;
     }
 
-    if (computerSelection == "rock" && playerSelection == "paper" || computerSelection == "scissors" && playerSelection == "rock" || computerSelection == "paper" && playerSelection == "scissors") {
-        playerScore++
-        plays++
+    if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")
+    ) {
+        computerScore++;
+        plays++;
+        winnerAlert.textContent = "Computer Wins Round!";
+        updateScore();
+        return 1;
     }
 
+    if (
+        (computerSelection == "rock" && playerSelection == "paper") ||
+        (computerSelection == "scissors" && playerSelection == "rock") ||
+        (computerSelection == "paper" && playerSelection == "scissors")
+    ) {
+        playerScore++;
+        plays++;
+        updateScore();
+        winnerAlert.textContent = "Player Wins Round!";
+        return 2;
+    }
 }
 
-function game() {
-    
+const buttons = document.querySelectorAll("button");
 
-    while (plays < 5) {
-        let playerSelection = prompt("Choice: ").toLowerCase();
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection))
-    }
-
-    if (computerScore > playerScore) {
-        console.log("computer wins")
-    } else {
-        console.log("player wins")
-    } 
-    console.log(computerScore, playerScore)
-}
-
-game()
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id);
+    });
+});
